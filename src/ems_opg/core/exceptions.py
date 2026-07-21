8f0686113bcaf60e1918449a73688059b425e2a6
zmcmillantesting <zmcmillan@myemssolutions.com>
2026-07-21 16:13:47 -0600

    created exceptions and added more to the list

--- src/ems_opg/core/exceptions.py
+++ src/ems_opg/core/exceptions.py
@@ -6,25 +6,92 @@ Custom application exceptions.
 
 
 class ApplicationError(Exception):
-    """Base application exception."""
+    """Base class for application exceptions."""
+    def __init__(self, message: str = ""):
+        super().__init__(message)
+        self.message = message
+
+    def __str__(self):
+        return self.message
 
 
 class ConfigurationError(ApplicationError):
     """Configuration problem."""
+    def __init__(self, message: str = ""):
+        super().__init__(message)
+        self.message = message
 
+    def __str__(self):
+        return self.message
 
 class DatabaseError(ApplicationError):
     """Database problem."""
+    def __init__(self, message: str = ""):
+        super().__init__(message)
+        self.message = message
 
+    def __str__(self):
+        return self.message
 
 class BackupError(ApplicationError):
     """Backup problem."""
+    def __init__(self, message: str = ""):
+        super().__init__(message)
+        self.message = message
 
+    def __str__(self):
+        return self.message
 
 class ValidationError(ApplicationError):
     """Validation failed."""
+    def __init__(self, message: str = ""):
+        super().__init__(message)
+        self.message = message
 
+    def __str__(self):
+        return self.message
 
 class WorkflowError(ApplicationError):
     """Workflow error."""
+    def __init__(self, message: str = ""):
+        super().__init__(message)
+        self.message = message
 
+class OrderNotFoundError(WorkflowError):
+    """Order not found."""
+    def __init__(self, message: str = ""):
+        super().__init__(message)
+        self.message = message
+
+    def __str__(self):
+        return self.message
+    
+class DeviceNotFoundError(WorkflowError):
+    """Device not found."""
+    def __init__(self, message: str = ""):
+        super().__init__(message)
+        self.message = message
+
+    def __str__(self):
+        return self.message
+    
+class MACImportError(WorkflowError):
+    """MAC address import error."""
+    def __init__(self, message: str = ""):
+        super().__init__(message)
+        self.message = message
+
+    def __str__(self):
+        return self.message
+
+class SchemaOutdatedError(WorkflowError):
+    """Database schema is outdated."""
+    def __init__(self, message: str = ""):
+        super().__init__(message)
+        self.message = message
+
+    def __str__(self):
+        return self.message
+
+    def __str__(self):
+        return self.message
