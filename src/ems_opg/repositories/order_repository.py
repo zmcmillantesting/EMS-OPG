883dc757ecf840f06697c57afdd02f0a45b5116b
zmcmillantesting <zmcmillan@myemssolutions.com>
2026-07-27 14:27:48 -0600

    updated order_repository.py to include a duplicate check for creation and a list all func

--- src/ems_opg/repositories/order_repository.py
+++ src/ems_opg/repositories/order_repository.py
@@ -14,7 +14,16 @@ class OrderRepository:
             )
         )
     
+    def list_all_orders(self):
+        return self.session.scalars(
+            select(Order)
+        ).all()
+    
     def create(self, order):
+        if self.get_by_order_number(order.order_number):
+            raise ValueError(
+                f"Order with order_number {order.order_number} already exists."
+            )
         self.session.add(order)
         self.session.commit()
 
